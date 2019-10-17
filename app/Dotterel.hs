e3a9c7dd694686c1143a2e0cd6548dd992f2ff82
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-10-17 17:18:53 -0400

    Add Dotterel.hs

---
+++ app/Dotterel.hs
@@ -0,0 +1,17 @@
+module Dotterel where
+
+import Dictionary
+import Keys
+import Sounds
+import Steno.Numbers
+import Stroke
+
+import qualified Keys.Left as L
+import qualified Keys.Right as R
+
+import Text.Printf
+
+
+dotterel = entries $
+  [entry "{^}{IME:EDITOR_ACTION}{^}{MODE:RESET}" [r <> r' <> b']
+  ,entry "=undo" [stk Star]]
