2d2e952e7ecbc2bdac58a6aabc706cf0b79ebd50
Elliot Robinson <elliot.robinson@erau.edu>
2019-11-15 14:02:41 -0500

    Enale/Disable dictionaries

--- app/Plover.hs
+++ app/Plover.hs
@@ -10,12 +10,32 @@ import qualified Keys.Left as L
 import qualified Keys.Right as R
 
 plover =
-  [Entry "{*+}" [stk Hash]
-  ,Entry "{*($c)}" [stk R.D <> stk R.Z <> stk Hash]
---  ,Entry "" [stk R.Z <> stk Hash]
-  ,Entry "{^}{-|}" [k <> p <> a]
-  ,Entry "{}{-|}" [k <> p <> a <> stk Star]
-  ,Entry "{^ ^}" [stk L.S <> stk R.P]
-  ,Entry "{}{^}" [stk L.S <> stk R.D]
-  ,Entry "{^}" [t <> k <> l' <> s']
+  let able = aa <> b' <> l'
+      nable = n <> able
+      dable = d <> able
+  in entries
+  [entry "{*+}"    [stk Hash]
+  ,entry "{*($c)}" [stk R.D <> stk R.Z <> stk Hash]
+--  ,entry "" [stk R.Z <> stk Hash]
+  ,entry "{^}{-|}" [k <> p <> a]
+  ,entry "{}{-|}"  [k <> p <> a <> star]
+  ,entry "{^ ^}"   [stk L.S <> stk R.P]
+  ,entry "{}{^}"   [stk L.S <> stk R.D]
+  ,entry "{^}"     [t <> k <> l' <> s']
+
+  -- Enable/disable
+  ,entry "{}"      [nable]
+  ,entry "{}"      [dable]
+  ,entry "{PLOVER:TOGGLE_DICT:+csharp.json}"
+         [nable, c <> sh']
+  ,entry "{PLOVER:TOGGLE_DICT:-csharp.json}"
+         [dable, c <> sh']
+  ,entry "{PLOVER:TOGGLE_DICT:+emacs.json}"
+         [nable, m <> a <> x']
+  ,entry "{PLOVER:TOGGLE_DICT:-emacs.json}"
+         [dable, m <> a <> x']
+  ,entry "{PLOVER:TOGGLE_DICT:+haskell.json}"
+         [nable, h <> a <> fvs' <> k']
+  ,entry "{PLOVER:TOGGLE_DICT:-haskell.json}"
+         [dable, h <> a <> fvs' <> k']
   ]
